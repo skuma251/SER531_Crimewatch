a71de77fd9e74a7915c2ceccdedf304ba39cdcb7
skuma251 <skuma251@asu.edu>
2023-11-06 13:34:34 -0700

    :art: Improve structure / format of the code.

---
+++ crimewatch/src/main/java/edu/asu/crimewatch/CrimewatchApplication.java
@@ -0,0 +1,13 @@
+package edu.asu.crimewatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimewatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimewatchApplication.class, args);
+	}
+
+}
---
+++ crimewatch/src/test/java/edu/asu/crimewatch/CrimewatchApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.asu.crimewatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimewatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
