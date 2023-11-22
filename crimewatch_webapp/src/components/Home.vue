4d1895ad7fe693212984c003851bc8b460ec596b
Subham Kumar <skuma251@asu.edu>
2023-11-22 15:24:11 -0700

    Updated Home page

--- crimewatch_webapp/src/components/Home.vue
+++ crimewatch_webapp/src/components/Home.vue
@@ -1,60 +1,145 @@
 <template>
-  <div class="flex relative pt-24 pb-16 px-4 sm:px-6 md:px-10">
-      <div class="w-1/2 px-4 sm:px-6 md:px-10">
-        <h1 class="text-8xl font-bold mb-4">
-          Welcome to the New York City Crimewatch
-        </h1>
-        <p class="text-2xl font-bold mb-4">
-          (A tool to analyze the New York City various crimes based on the past data)
+  <div class="flex px-4 pt-10 pb-10">
+    <div class="w-1/2 md:px-0">
+      <h1 class="text-4xl font-bold">NYC CrimeWatch</h1>
+      <p class="md:text-xl">
+        A tool to analyze various crimes in New York City based on past data
+      </p>
+      <div class="mt-8">
+        <p>
+          This application is a data-driven web platform focused on analyzing
+          crime-related data in New York City. It specializes in examining
+          shooting incidents and hate crimes. By utilizing advanced technologies
+          like Semantic Web Engineering, ontologies, and RDF, the application
+          provides insights crucial for decision-making and proactive crime
+          prevention.
         </p>
       </div>
-
-      <div class="w-full lg:w-2/3 xl:w-1/2 relative px-2">
-      <div class="overflow-hidden h-full">
-        <div class="w-full h-full flex">
-          <!-- Images -->
-          <div
-            v-for="(image, index) in images"
-            :key="index"
-            :class="{ 'ml-0': index === 0, 'ml-10': index !== 0 }"
-            class="w-full flex-shrink-0 transition-transform duration-500"
-            :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
+      <div class="mt-8">
+        <h2 class="text-2xl font-semibold">Key Functionalities</h2>
+        <ul class="list-disc mt-4 ml-6">
+          <li>
+            <span class="font-bold">Racial Disparity Analysis:</span> Examining
+            shooting incident data by race for both perpetrators and victims.
+          </li>
+          <li>
+            <span class="font-bold">Temporal Patterns:</span> Providing insights
+            into the timing of shooting incidents to identify temporal patterns.
+          </li>
+          <li>
+            <span class="font-bold">Prevalence Ratio:</span> Calculating and
+            displaying the ratio of shooting incidents relative to total
+            reported crimes.
+          </li>
+          <li>
+            <span class="font-bold">Demographic Analysis: </span>Analyzing
+            shooting incident ratios relative to factors like race, age, or
+            borough.
+          </li>
+          <li>
+            <span class="font-bold">Hate Crime Insights: </span>Providing
+            detailed insights into hate crimes by location, time, demographics,
+            and more.
+          </li>
+        </ul>
+        <p class="mt-4">
+          The application aims to empower stakeholders by presenting valuable
+          visualizations and analytical results. It allows for informed
+          decision-making, resource allocation, and proactive measures to
+          address interconnected crime issues in New York City.
+        </p>
+      </div>
+      <div class="mt-4">
+        <p>
+          <span>To download and study the full report, </span>
+          <a
+            href="src/assets/Crimewatch.pdf"
+            download="CrimewatchReport.pdf"
+            class="text-blue-500 hover:underline"
           >
-            <img :src="image" alt="image" class="w-full h-full object-cover" />
-          </div>
+            click here
+          </a>
+        </p>
+      </div>
+    </div>
+    <div class="w-full xl:w-1/2 relative overflow-hidden">
+      <div class="flex">
+        <div
+          v-for="(image, index) in crimeImages"
+          :key="index"
+          :class="{ 'ml-10': index !== 0 }"
+          class="w-full flex-shrink-0 transition-transform duration-500"
+          :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
+        >
+          <img
+            :src="image"
+            :alt="`image_${index}`"
+            class="w-full h-full object-cover"
+          />
         </div>
       </div>
-
-
-      <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-        <span
-          v-for="(image, index) in images"
-          :key="index"
-          :class="{ 'bg-gray-800': currentIndex === index, 'bg-gray-300': currentIndex !== index }"
-          class="h-3 w-3 rounded-full cursor-pointer"
-          @click="changeImage(index)"
-        ></span>
+      <h2 class="text-2xl font-bold mt-8 text-center">Crime News Alert !</h2>
+      <div class="card w-full bg-base-100 shadow-xl">
+        <div class="card-body">
+          <transition name="fade">
+            <h2 class="card-title" :key="currentNews?.title">
+              {{ currentNews?.title }}
+            </h2>
+          </transition>
+          <transition name="slide">
+            <div class="flex flex-col" :key="currentNews?.title">
+              <p class="mb-4">{{ currentNews?.description }}</p>
+              <p class="text-right">{{ currentNews?.author }}</p>
+            </div>
+          </transition>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import gunCrime from '@/images/gun_crime.jpg';
-import hateCrime from '@/images/hate_crime.jpg';
-import hateCrime2 from '@/images/hate_crime2.jpg';
-
+import { ref, computed, onMounted } from "vue";
+import { fetchNews } from "../utils/api";
+import { NewsArticle } from "../utils/types";
+import gunCrime from "@/assets/images/gun_crime.jpg";
+import hateCrime from "@/assets/images/hate_crime.jpg";
+import hateCrime2 from "@/assets/images/hate_crime2.jpg";
 const currentIndex = ref(0);
-const intervalDuration = 4000; // Change image every 4 seconds
+const intervalDuration = 4000;
+const crimeImages = [gunCrime, hateCrime, hateCrime2];
+const imageCount = computed(() => crimeImages.length);
+const newsCount = computed(() => newsArticles.value.length);
+const newsArticles = ref<NewsArticle[]>([]);
+const currentNews = ref<NewsArticle>();
+const currentNewsIndex = ref(0);
 
-function changeImage(index: number) {
-  currentIndex.value = index;
+function startImageRotation() {
+  setInterval(() => {
+    currentIndex.value = (currentIndex.value + 1) % imageCount.value;
+  }, intervalDuration);
 }
 
-const images = [gunCrime, hateCrime, hateCrime2]; // Use the imported images
+function startNewsRotation() {
+  setInterval(() => {
+    currentNewsIndex.value = (currentNewsIndex.value + 1) % newsCount.value;
+  }, intervalDuration);
+}
 
-setInterval(() => {
-  currentIndex.value = (currentIndex.value + 1) % images.length;
-}, intervalDuration);
+async function fetchNYNews() {
+  try {
+    const articles = await fetchNews();
+    newsArticles.value = articles;
+    currentNews.value = newsArticles.value[currentNewsIndex.value];
+    console.log(currentNews.value);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+  }
+}
+
+onMounted(() => {
+  startImageRotation();
+  startNewsRotation();
+  fetchNYNews();
+});
 </script>
--- crimewatch_webapp/src/components/NavBar/index.vue
+++ crimewatch_webapp/src/components/NavBar/index.vue
@@ -1,29 +1,31 @@
+<template>
+  <header
+    class="navbar bg-primary min-h-fit h-12 flex items-center justify-between"
+  >
+    <div class="flex items-center">
+      <router-link to="/" class="flex items-center text-white">
+        <h1 class="text-xl">CrimeWatch</h1>
+        <Icon icon="ic:baseline-home" class="ml-4 text-2xl" />
+      </router-link>
+    </div>
+    <div class="hidden md:flex items-center justify-center flex-1">
+      <nav class="menu menu-horizontal space-x-4 text-white">
+        <li>
+          <router-link to="/complaints" aria-current="page"
+            >Complaints</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/shooting">Shooting</router-link>
+        </li>
+        <li>
+          <router-link to="/hate-crime">Hate Crime</router-link>
+        </li>
+      </nav>
+    </div>
+  </header>
+</template>
+
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
 </script>
-
-<template>
-  <nav class="navbar bg-primary min-h-fit h-12 flex items-center justify-between">
-    <!-- Left-aligned content -->
-    <div class="flex items-center">
-      <span class="text-white font-custom text-xl">CrimeWatch</span>
-      <router-link to="/">
-        <div class="ml-4">
-          <Icon icon="ic:baseline-home" class="text-white text-2xl" />
-        </div>
-      </router-link>
-    </div>
-
-    <!-- Center-aligned content -->
-    <div class="flex items-left">
-     <ul class="menu menu-horizontal">
-           <li><a>Complaints</a></li>
-           <li><a>Shooting</a></li>
-           <li><a>Hate Crime</a></li>
-         </ul>
-    </div>
-
-    <div class="flex items-center">
-    </div>
-  </nav>
-</template>
