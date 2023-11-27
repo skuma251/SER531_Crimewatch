0a84111ce3ad2135a00414175cbfbd4df1bb9211
amarjeet-singh-1304 <asing499@asu.edu>
2023-11-27 11:02:41 -0700

    Adding the Apis for the hate crime

---
+++ crimewatch/src/main/java/edu/asu/crimewatch/service/HateCrimeService.java
@@ -0,0 +1,87 @@
+package edu.asu.crimewatch.service;
+
+import edu.asu.crimewatch.helper.OWLReader;
+
+import org.springframework.stereotype.Service;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collections;
+
+@Service
+
+public class HateCrimeService{
+
+    static String hateCrimeURL = "http://localhost:3030/HateCrime_Data";
+
+    public List<Integer> fetchYearValues(){
+
+        String query = "SELECT DISTINCT ?YEAR WHERE {"+
+                        "?crime rdf:type crimewatch:Crime; "+
+                        "crimewatch:occuredDateTime ?datetime; "+
+                        "BIND(YEAR(?datetime) AS ?YEAR)."+
+                        "FILTER(?YEAR != 2023)."+
+                        "}";
+
+        ResultSet response = OWLReader.runSparQLQuery(hateCrimeURL, query);
+        List<Integer> yearList = new ArrayList<>();
+
+        while(response.hasNext()) {
+            QuerySolution soln = response.nextSolution();
+            int year = (int) soln.getLiteral("?YEAR").getValue();
+            yearList.add(year);           
+        }
+        Collections.sort(yearList);
+        return yearList;
+    }
+
+    public Map<String,Integer> getBoroCountByYear(int year){
+
+        String query = "SELECT ?BoroName (COUNT( Distinct ?crime) as ?Count) WHERE {"+
+                        "?crime rdf:type crimewatch:Crime ;"+
+                        "crimewatch:occuredIn ?Location;"+
+                        "crimewatch:occuredDateTime ?datetime;"+
+                        "BIND(YEAR(?datetime) AS ?year)."+
+                        "?Location crimewatch:underArea ?Boro."+
+                        "?Boro crimewatch:hasName ?BoroName."+
+                        "FILTER(?year =" + year +")."+
+                        "} GROUP BY ?BoroName";
+
+        ResultSet response = OWLReader.runSparQLQuery(hateCrimeURL, query);
+        Map<String, Integer> boroCountMap = new HashMap<>();
+         while( response.hasNext())
+        {
+
+            QuerySolution soln = response.nextSolution();
+            boroCountMap.put(soln.getLiteral("?BoroName").getString(),soln.getLiteral("?Count").getInt());
+        }
+      return boroCountMap;
+    }
+
+    public Map<String,Integer> getBiasCountByYear(int year){
+
+        String query = "SELECT ?BiasMotive (COUNT( Distinct ?crime) as ?Count) WHERE {"+
+                        "?crime rdf:type crimewatch:Crime ;"+
+                        "crimewatch:occuredDateTime ?datetime;"+
+                        "crimewatch:hasBiasMotiveDesc ?BiasMotive."+
+                        "BIND(YEAR(?datetime) AS ?year)."+
+                        "FILTER(?year =" + year +")."+
+                        "} GROUP BY ?BiasMotive";
+
+        ResultSet response = OWLReader.runSparQLQuery(hateCrimeURL, query);
+        Map<String, Integer> biasCountMap = new HashMap<>();
+         while( response.hasNext())
+        {
+
+            QuerySolution soln = response.nextSolution();
+            biasCountMap.put(soln.getLiteral("?BiasMotive").getString(),soln.getLiteral("?Count").getInt());
+        }
+      return biasCountMap;
+
+    }
+
+}
