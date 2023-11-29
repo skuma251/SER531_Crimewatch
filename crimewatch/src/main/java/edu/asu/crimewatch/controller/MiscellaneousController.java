7341e624677083edc5859360c1fb77efb247f8e8
amarjeet-singh-1304 <asing499@asu.edu>
2023-11-29 10:54:24 -0700

    Adding the miscalleneoous API

---
+++ crimewatch/src/main/java/edu/asu/crimewatch/controller/MiscellaneousController.java
@@ -0,0 +1,28 @@
+package edu.asu.crimewatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.asu.crimewatch.service.MiscellaneousService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/miscellaneous")
+public class MiscellaneousController {
+
+    @Autowired
+    MiscellaneousService miscellaneousService;
+    
+    @GetMapping("/getComplaintShootingCountByYear")
+    ResponseEntity<?> getCrimeTypeCount(@RequestParam("year") int year) {
+        Map<String, List<Integer>>  complaintShootingCount = miscellaneousService.fetchShootingHateCountByYear(year);
+        return ResponseEntity.ok(complaintShootingCount);
+    }
+}
